891a234e223be3df0f9aa07baf549c26d02cbfc9
M. Taufik R <35796729+wunsc@users.noreply.github.com>
2018-01-29 16:47:05 +0700

    fix

--- src/tugascasei/DateComparator.java
+++ src/tugascasei/DateComparator.java
@@ -46,38 +46,3 @@ public class DateComparator {
         return hari;
     }
 }
-
-//        int thna = 0;
-//        int thnb = 0;
-//        int blna = 0;
-//        int blnb = 0;
-//        int thna = Integer.valueOf(a.substring(5));
-//        int thnb = Integer.valueOf(b.substring(5));
-//        int blna = Integer.valueOf(a.substring(3, 4));
-//        int blnb = Integer.valueOf(b.substring(3, 4));
-//        int hra = Integer.valueOf(a.substring(0, 2));
-//        int hrb = Integer.valueOf(b.substring(0, 2));
-//        if ((a.substring(4, 5) == "0" || a.substring(4, 5) == "1") && (b.substring(4, 5) == "0" || b.substring(4, 5) == "1")){
-//            blna = Integer.valueOf(a.substring(3, 5)); 
-//            blnb = Integer.valueOf(b.substring(3, 5));
-//            thna = Integer.valueOf(a.substring(6));
-//            thnb = Integer.valueOf(b.substring(6));
-//        } else if ((a.substring(4, 5) == "-") && (b.substring(4, 5) == "0" || b.substring(4, 5) == "1")) {
-//            blna = Integer.valueOf(a.substring(3, 4));
-//            blnb = Integer.valueOf(b.substring(3, 5));
-//            thna = Integer.valueOf(a.substring(5));
-//            thnb = Integer.valueOf(b.substring(6));
-//        } else if ((a.substring(4, 5) == "0" || a.substring(4, 5) == "1") && (b.substring(4, 5) == "-")) {
-//            blna = Integer.valueOf(a.substring(3, 5));
-//            blnb = Integer.valueOf(b.substring(3, 4));
-//            thna = Integer.valueOf(a.substring(6));
-//            thnb = Integer.valueOf(b.substring(5));
-//        } else if ((a.substring(4, 5) == "-") && (b.substring(4, 5) == "-")){
-//            blna = Integer.valueOf(a.substring(3, 4));
-//            blnb = Integer.valueOf(b.substring(3, 4));
-//            thna = Integer.valueOf(a.substring(5));
-//            thnb = Integer.valueOf(b.substring(5));
-//        }
-//        aw.set(thna, blna, hra);
-//        ak.set(thnb, blnb, hrb);
-
--- src/tugascasei/Main.java
+++ src/tugascasei/Main.java
@@ -5,16 +5,10 @@
  */
 package tugascasei;
 
-//import com.sun.javafx.scene.control.skin.VirtualFlow;
 import java.text.ParseException;
-//import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-//import java.util.Calendar;
-//import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
 
 /**
  *
@@ -26,7 +20,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         long selisih_hari;
         MyDate md = new MyDate();
         List<MyDate> obdat = new ArrayList<MyDate>();
@@ -66,41 +60,3 @@ public class Main {
         }
     }
 }
-        
-//        for (MyDate myDate : obdat) {
-//            //dc.Comparator(myDate, myDate);
-//            System.out.println(myDate.toString());
-//            //System.out.println(myDate.toString().length());
-//        }
-        
-//        System.out.println(obdat.get(0).toString());
-//        String a = obdat.get(0).toString();
-//        SimpleDateFormat sd = new SimpleDateFormat("dd-MM-yyyy");
-//        try {
-//            Date d = sd.parse(a);
-//            System.out.println(sd.format(d));
-//        String b = a.substring(4, 5);
-//        String c = a.substring(5);
-//        System.out.println(b+c);
-//System.out.println(selisih_hari);
-//        String b = obdat.get(0).toString();
-//        System.out.println(b);
-//        System.out.println(b.substring(5));
-//        System.out.println(b.substring(3, 4));
-//        System.out.println(b.substring(0, 2));
-//        
-//        Calendar cld = Calendar.getInstance();
-//        Calendar cld1 = Calendar.getInstance();
-//        //System.out.println(cld.get(Calendar.DAY_OF_YEAR));
-//        
-//        cld.set(2018, 2, 30);
-//        cld1.set(2018, 2, 28);
-//        int a = cld1.compareTo(cld);
-//        System.out.println(a);
-//        int day = cld.get(Calendar.DAY_OF_WEEK);
-//        System.out.println(day);
-//        int max = cld.getActualMaximum(Calendar.DAY_OF_MONTH);
-//        System.out.println(max);
-//        } catch (ParseException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
